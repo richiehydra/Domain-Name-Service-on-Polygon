bf62648b7a2dbd39e01f82b5ba0ab11b66f81243
Keshav Poojary <110924969+richiehydra@users.noreply.github.com>
2023-02-12 22:31:50 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,6 @@ module.exports = {
     mumbai:
     {
       url:"https://polygon-mumbai.g.alchemy.com/v2/8-UneshwPpMnoJfhWcPaT-uTM9QHlpWA",
-      accounts:["8a4f69299d51bc57c82246e00f914eea83b4719de46c56e3bcbb3aef6d5251f4"],
-    }
+      accounts:["Private Key"],
   }
 };
